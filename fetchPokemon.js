fee0e3eff924531687fef8653078ddfc52bba484
Joseph Reyes <jreyes1919@gmail.com>
2024-02-24 21:32:59 -0600

    added downloadSprites
    Added function to download sprites from pokemon into the same folder as stats. Creates a new folder if the folder doesn't already exist.

--- fetchPokemon.js
+++ fetchPokemon.js
@@ -1,5 +1,8 @@
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Gets url for pokemon from prompt_poke_name() input
 const getPokemonData = async (selected_pokemon, selected_info) => {
   const chosen_pokemon = Object.values(selected_pokemon)[0].toLowerCase();
@@ -8,15 +11,16 @@ const getPokemonData = async (selected_pokemon, selected_info) => {
   const data = await response.json();
   //   console.log(data);
   //   console.log(selected_info);
+  const folderName = `${chosen_pokemon}`;
+  const folderPath = path.join(__dirname, folderName);
   const info_array = Object.values(selected_info)[0];
   info_array.forEach((info) => {
     switch (info) {
       case "Stats":
-        writeStats(chosen_pokemon, data);
+        downloadStats(folderPath, data);
         break;
       case "Sprites":
-        console.log("=====SPRITES=====");
-        console.log(data.sprites);
+        downloadSprites(folderPath, data);
         break;
       case "Artwork":
         console.log("=====ARTWORK=====");
@@ -26,19 +30,56 @@ const getPokemonData = async (selected_pokemon, selected_info) => {
   });
 };
 
-const writeStats = (chosen_pokemon, data) => {
-  let statFile = "";
-  data.stats.forEach((statObject) => {
-    const { base_stat, stat } = statObject;
-    statFile += `${stat.name}: ${base_stat}\n`;
-  });
-  console.log(`Saved: ${chosen_pokemon}/stats.txt`);
-  return fs.writeFile(`stats.txt`, statFile, (err) => {
-    if (err) {
-      console.error(err);
+const downloadStats = async (folderPath, data) => {
+  const fileName = `stats.txt`;
+  const filePath = path.join(folderPath, fileName);
+
+  try {
+    await fs.mkdir(folderPath, { recursive: true });
+    let statFile = "";
+    data.stats.forEach((statObject) => {
+      const { base_stat, stat } = statObject;
+      statFile += `${stat.name}: ${base_stat}\n`;
+    });
+
+    await fs.writeFile(filePath, statFile);
+    console.log(`Saved: ${fileName}`);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+};
+
+const downloadSprites = async (folderPath, data) => {
+  const { other, versions, ...filteredSprites } = data.sprites;
+
+  try {
+    await fs.access(folderPath);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      try {
+        await fs.mkdir(folderPath, { recursive: true });
+      } catch (err) {
+        console.error("Error creating folder:", err);
+        return;
+      }
+    } else {
+      console.error("Error accessing folder:", error);
       return;
     }
-  });
+  }
+
+  await Promise.all(
+    Object.entries(filteredSprites).map(async ([key, value]) => {
+      if (value !== null) {
+        const response = await fetch(value);
+        const imageBuffer = await response.arrayBuffer();
+        const imageFileName = `${key}.png`;
+        const imagePath = path.join(folderPath, imageFileName);
+        await fs.writeFile(imagePath, Buffer.from(imageBuffer));
+        console.log(`Saved: ${imageFileName}`);
+      }
+    })
+  );
 };
 
 export { getPokemonData };
